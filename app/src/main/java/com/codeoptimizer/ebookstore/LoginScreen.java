1b6228da72905e09504fb484d576f1b4f57368e7
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-14 17:52:19 -0400

    [Ankita Pabbi] Updated LoginScreen Layout, also update style.xml for further use.

--- app/src/main/java/com/codeoptimizer/ebookstore/LoginScreen.java
+++ app/src/main/java/com/codeoptimizer/ebookstore/LoginScreen.java
@@ -3,13 +3,31 @@ package com.codeoptimizer.ebookstore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity {
 
+    EditText userEmail,userPassword;
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        userEmail = (EditText)findViewById(R.id.userEmail);
+        userPassword = (EditText)findViewById(R.id.userPassword);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = userEmail.getText().toString();
+                String password = userPassword.getText().toString();
+
+                
+            }
+        });
 
     }
 }
