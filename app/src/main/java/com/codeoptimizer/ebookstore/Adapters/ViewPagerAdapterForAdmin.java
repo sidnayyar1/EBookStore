7382d7390340dd8ff0feb923a5e575dc25bb4e65
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-15 19:43:20 -0400

    [Ankita Pabbi] Created seprate layout for a user and admin also added  custom tabbed Layout for admin and Navigation Layout for the user.

---
+++ app/src/main/java/com/codeoptimizer/ebookstore/Adapters/ViewPagerAdapterForAdmin.java
@@ -0,0 +1,47 @@
+package com.codeoptimizer.ebookstore.Adapters;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.codeoptimizer.ebookstore.Fragments.AddBook;
+import com.codeoptimizer.ebookstore.Fragments.DeleteBook;
+import com.codeoptimizer.ebookstore.Fragments.UpdateBook;
+import com.codeoptimizer.ebookstore.Fragments.ViewFragment;
+
+import java.util.ArrayList;
+
+public class ViewPagerAdapterForAdmin extends FragmentStatePagerAdapter {
+    int mNumOfTabs;
+
+    public ViewPagerAdapterForAdmin(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.mNumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                ViewFragment tab1 = new ViewFragment();
+                return tab1;
+            case 1:
+                AddBook tab2 = new AddBook();
+                return tab2;
+            case 2:
+                DeleteBook tab3 = new DeleteBook();
+                return tab3;
+            case 3:
+                UpdateBook tab4 = new UpdateBook();
+                return tab4;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mNumOfTabs;
+    }
+}
--- app/src/main/java/com/codeoptimizer/ebookstore/HomeScreenForAdmin.java
+++ app/src/main/java/com/codeoptimizer/ebookstore/HomeScreenForAdmin.java
@@ -1,14 +1,83 @@
 package com.codeoptimizer.ebookstore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class HomeScreenForAdmin extends AppCompatActivity {
+import com.codeoptimizer.ebookstore.Adapters.ViewPagerAdapterForAdmin;
+import com.codeoptimizer.ebookstore.Fragments.AddBook;
+import com.codeoptimizer.ebookstore.Fragments.DeleteBook;
+import com.codeoptimizer.ebookstore.Fragments.UpdateBook;
+import com.codeoptimizer.ebookstore.Fragments.ViewFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeScreenForAdmin extends AppCompatActivity implements
+        ViewFragment.OnFragmentInteractionListener,
+        AddBook.OnFragmentInteractionListener,
+        DeleteBook.OnFragmentInteractionListener,
+        UpdateBook.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
+        setContentView(R.layout.activity_home_screen_for_admin);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 4"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new ViewPagerAdapterForAdmin
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      //  getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
