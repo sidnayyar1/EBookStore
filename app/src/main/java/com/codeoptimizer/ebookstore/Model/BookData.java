7d8a4e0198420ea720df0c951dc35a2529002dc3
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-16 15:45:41 -0400

    [Ankita Pabbi] Created BookData model for further use.

---
+++ app/src/main/java/com/codeoptimizer/ebookstore/Model/BookData.java
@@ -0,0 +1,49 @@
+package com.codeoptimizer.ebookstore.Model;
+
+public class BookData {
+    private String bookName;
+    private String bookAuthor;
+    private String bookDesc;
+    private String bookUrl;
+    private String bookCategory;
+
+    public String getBookCategory() {
+        return bookCategory;
+    }
+
+    public void setBookCategory(String bookCategory) {
+        this.bookCategory = bookCategory;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookDesc() {
+        return bookDesc;
+    }
+
+    public void setBookDesc(String bookDesc) {
+        this.bookDesc = bookDesc;
+    }
+
+    public String getBookUrl() {
+        return bookUrl;
+    }
+
+    public void setBookUrl(String bookUrl) {
+        this.bookUrl = bookUrl;
+    }
+}
