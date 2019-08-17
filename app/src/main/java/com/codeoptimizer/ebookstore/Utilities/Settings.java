3bcafa799b19f83b1bbb95bf5f025db5a8a3587b
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-16 21:02:51 -0400

    [Sidharth] Added setting fragment and logout Button.

---
+++ app/src/main/java/com/codeoptimizer/ebookstore/Utilities/Settings.java
@@ -0,0 +1,16 @@
+package com.codeoptimizer.ebookstore.Utilities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codeoptimizer.ebookstore.R;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
