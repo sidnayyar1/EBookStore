0b5c3702d171c18141dd42de1193a0374ac14d57
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-19 16:15:40 -0400

    [Ankita Pabbi] Added added Splash screen for the project also added its logic.

--- app/src/main/java/com/codeoptimizer/ebookstore/SplashScreen.java
+++ app/src/main/java/com/codeoptimizer/ebookstore/SplashScreen.java
@@ -2,7 +2,9 @@ package com.codeoptimizer.ebookstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,6 +14,14 @@ public class SplashScreen extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),LoginScreen.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
     }
 
 }
